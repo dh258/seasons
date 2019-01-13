da3e9013b270c12f535d7267ae91fe1c2d20b79f
Darell Hoei <darellhoei@gmail.com>
2019-01-12 19:18:21 -0800

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = { lat: null, errMsg: '' };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => this.setState({ lat: position.coords.latitude }),
+        (err) => this.setState({ errMsg: err.message })
+
+    );
+  }
+
+  renderContent() {
+    if (this.state.errMsg && !this.state.lat) {
+      return <div>Error: {this.state.errMsg}</div>;
+    }
+
+    if (!this.state.errMsg && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>;
+    }
+
+    return <Loader msg="Waiting for location permission..."/>;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
